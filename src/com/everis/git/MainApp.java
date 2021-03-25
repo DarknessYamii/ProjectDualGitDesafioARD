892b05cd7385cdab10d76d12248d465b76290b9a
aruidelg <aruidelg@everis.com>
2021-03-25 09:08:57 +0100

    Version 1 - GIT

---
+++ src/com/everis/git/MainApp.java
@@ -0,0 +1,22 @@
+package com.everis.git;
+
+/**
+ * Desafio GIT
+ * @author aruidelg
+ *
+ */
+
+public class MainApp {
+
+	/**
+	 * Metodo main 
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hola mundo");
+		
+	}
+
+}
